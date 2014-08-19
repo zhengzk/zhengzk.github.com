eb0037d6b527243d047aaa5c8c6d0f404230bae5
zhengzk <studyc@163.com>
2014-08-19 09:01:02 +0800

    添加功能
    添加关于我tab

--- static/service/index.js
+++ static/service/index.js
@@ -1,3 +1,34 @@
+base.addScript(RELATIVE_PATH+'service/module/resume-table.js'); 
 $(function(){
+	var tbCon = $('#tb')[0];
+	var naviCon = $('.navi')[0];
+	
+	var tb = new base.ResumeTable();
+	$(tbCon).append(tb.getRoot());
+	tb.reflushData(base.ResumeTable.Data);
+	
+    var navi_data = [
+		{data:{label:'关于我',index:0}},
+		{data:{label:'个人简历预览',index:1}}
+	];
+	
+	var select = function(inx,ele,data){
+		$('.content').each(function(i){
+			if(inx == 0){
+				tb.init();
+			}
+			if(i==inx){
+				$(this).show();
+			}else{
+				$(this).hide();
+			}
+		});
+	};
 
-});
+	var t = new base.component.TabNavi({
+		cb_select:select,
+		default_index:1,
+	});
+	$(naviCon).append(t.getRoot());
+	t.reflushData(navi_data);
+})
